8cec186774959ac7d6fd18f1dc528c411c522655
N-Pex <npexdev@gmail.com>
2019-06-19 08:46:07 +0200

    Rename to RFA for now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,7 +21,7 @@
           </v-list-tile-avatar>
 
           <v-list-tile-content>
-            <v-list-tile-title>NUUS</v-list-tile-title>
+            <v-list-tile-title>RFA</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
